d9703a5b7d2b5bb0005f2a0067ca4da4a77ac7c6
Gabriela Payano <gabipayano@Gabrielas-MacBook-Air.local>
2023-02-06 18:08:20 -0800

    adding in div elements

---
+++ app.js
@@ -0,0 +1,43 @@
+const app = Vue.createApp({
+    //template
+    //template: '<h2>Whataaa</h2>'
+    data(){
+        return{
+            showBook: true,
+            title: "Book Harry Potter : JK",
+            age:33,
+            blob: "dsv",
+            x:0,
+            y:0,
+            books: [
+                { title: 'wonder woman', author: 'dc-comic', img: 'assets/1.jpg', isFav:true},
+                {title: 'wonder ant', author: 'dc-comic', img: 'assets/1.jpg', isFav:true},
+                {title: 'wonder wo', author: 'dc-comic', img: 'assets/1.jpg', isFav:true}
+
+            ]
+        }
+    },
+    methods: {
+        // changeTitle(title){
+        //    // this.title = "The Spare"
+        //    this.title = title
+        // },
+        toggleBooks(){
+            this.showBook = !this.showBook
+
+        },
+        handleEvent(e, data){
+            console.log(e, e.type)
+            if(data){
+                console.log(data)
+            }
+        },
+        handleMouse(e){
+            this.x = e.offsetX,
+            this.y = e.offsetY
+
+        }
+    }
+})
+
+app.mount('#app')
